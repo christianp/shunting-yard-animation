d6be5888c72e948f8f3609623a7166fa0353a859
Christian Lawson-Perfect <christianperfect@gmail.com>
2022-06-03 15:27:34 +0100

    some comments in Token.elm

--- src/Token.elm
+++ src/Token.elm
@@ -1,12 +1,20 @@
-module Token exposing (Token(..), Expression(..), BinaryOp, toString, tokenise, ops)
+module Token exposing (Token(..), BinaryOp, toString, tokenise, ops)
 
 import Parser as P exposing (Parser, (|.), (|=))
 import Set
 
 {-
 Types and functions for parsing and manipulating strings of tokens.
+
+The idea is to parse a string containing a mathematical expression like ``1 + 2^(3-4)`` into a string of tokens representing each of the pieces of the expression, so that they can be manipulated.
 -}
 
+{-
+A representation of a binary operator.
+
+The precedence determines the order of operations.
+A lower precedence number means that the operator should be evaluated first.
+-}
 type alias BinaryOp =
     { symbol : String
     , precedence : Int
@@ -14,6 +22,11 @@ type alias BinaryOp =
     , display : String
     }
 
+{-
+Kinds of token.
+
+The ``Comma`` token is in there because it'd be necessary for n-ary function calls, but I didn't implement that.
+-}
 type Token
     = Name String
     | LiteralNumber Int
@@ -22,12 +35,9 @@ type Token
     | RightBracket
     | Comma
 
-type Expression
-    = Atom Token
-    | BinaryOpApplication Expression Expression
-    | FunctionApplication (List Expression)
-
-
+{-
+Represent a token as a string. (the opposite of parsing)
+-}
 toString : Token -> String
 toString t = case t of
     Name name -> name
@@ -37,9 +47,15 @@ toString t = case t of
     RightBracket -> ")"
     Comma -> ", "
 
+{-
+Parse an operator: for each of the defined operators, if its symbol is next, then return a token representing that operation.
+-}
 parse_op : Parser Token
 parse_op = P.oneOf <| List.map (\t -> P.succeed (BinaryOpToken t) |. P.symbol t.symbol) ops
 
+{-
+Parse a single token.
+-}
 parse_token : Parser Token
 parse_token = P.oneOf
     [ P.succeed LeftBracket |. P.symbol "("
@@ -50,6 +66,9 @@ parse_token = P.oneOf
     , parse_op
     ]
 
+{-
+A parser which produces a list of tokens.
+-}
 tokeniser : Parser (List Token)
 tokeniser = P.sequence
     { start = ""
@@ -60,9 +79,17 @@ tokeniser = P.sequence
     , trailing = P.Optional
     } |. P.end
 
+{-
+Execute the parser on a given string.
+
+If it can't be parsed, the returned error is a list of points at which the parser got stuck.
+-}
 tokenise : String -> Result (List P.DeadEnd) (List Token)
 tokenise = P.run tokeniser
 
+{-
+The defined operations.
+-}
 ops : List BinaryOp
 ops = 
     [ {display = "×", symbol = "*", precedence = 1, leftAssociative = True}
